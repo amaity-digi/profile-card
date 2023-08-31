4d635181d34cb76735f260bb144316861a8c827b
amaity-digi <116334087+amaity-digi@users.noreply.github.com>
2023-08-31 17:14:38 +0530

    profile and skills added

--- src/App.js
+++ src/App.js
@@ -14,19 +14,44 @@ function App() {
 
 function Avatar() {
   return (
-    <img src='photo.jpg' alt='avatar' />
+    <div >
+      <img className='avatar' src='profile.jpg' alt='avatar' />
+    </div>
+
   )
 }
 
 function Intro() {
   return (
-    <h2>Hello</h2>
+    <div className='data'>
+      <h2>Ashish Maity</h2>
+      <p>Full Stack web developer and strong knowledge on JavaScript, React ,Java, SQL. Next will be learn Next.js and Typescript as well.</p>
+    </div>
+
   )
 }
 
 function SkillList() {
   return (
-    <h2>Hello</h2>
+    <div className='skill-list'>
+      <Skill skill="JavaScript" emoji="👌" color="#888" />
+      <Skill skill="React" emoji="😎" color="Yellow" />
+      <Skill skill="HTML + CSS" emoji="😎" color="red" />
+      <Skill skill="SQL" emoji="😎" color="purple" />
+      <Skill skill="Java" emoji="👍" color="green" />
+      <Skill skill="Mongodb" emoji="🤞" color="orange" />
+    </div>
+  )
+}
+
+function Skill(props) {
+  const { skill, emoji, color } = props
+
+  return (
+    <div className='skill' style={{ backgroundColor: color }}>
+      <span>{skill}</span>
+      <span>{emoji}</span>
+    </div>
   )
 }
 
